ab20f2f5f4e2585422449da231bf159bdfdb88e4
Yash <yashkad412@gmail.com>
2025-03-16 02:09:59 +0530

    updated eslit confing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-any": "off",
     },
